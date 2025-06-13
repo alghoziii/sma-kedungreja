dbd26310de473639ef23f52dd7a9514d17317566
alghozii_zyy <shobarudin67@students.amikom.ac.id>
2025-06-13 13:45:43 +0700

    osis

--- src/views/siswa/Osis.vue
+++ src/views/siswa/Osis.vue
@@ -1,3 +1,14 @@
 <template>
-    
-</template>
+    <section class="mx-auto max-w-6xl px-4 py-8">
+      <div class="space-y-8">
+        <h2 class="text-4xl font-bold">Struktur Organisasi Osis</h2>
+        <div class="flex justify-center">
+          <img
+            src="/strukturorganisasi.png"
+            alt="Struktur Organisasi Osis"
+            class="w-full max-w-md md:max-w-lg lg:max-w-xl h-auto"
+          />
+        </div>
+      </div>
+    </section>
+  </template>
